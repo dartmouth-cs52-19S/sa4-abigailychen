26a37ce778c37c61d53454302adb35fffb0c2cef
Abigail Chen <abigail.y.chen.17@dartmouth.edu>
2019-04-12 22:52:49 -0400

    counts seconds

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import $ from 'jquery';
 import './style.scss';
 
-$('#main').html('Here we go!');
+let seconds = 0;
+const printFunction = () => {
+  $('#main').html(`You have been on this page for ${seconds} seconds.`);
+  seconds += 1;
+};
+
+setInterval(printFunction, 1000);
