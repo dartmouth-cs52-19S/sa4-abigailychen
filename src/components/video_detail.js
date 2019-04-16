0470a4b244057090bb8d376ab3bc01a43681ae14
Abigail Chen <abigail.y.chen.17@dartmouth.edu>
2019-04-16 01:58:51 -0400

    final

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -4,14 +4,14 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   } else {
-    const videoId = video.id.videoId; // will give linting error - read it and decide for yourself
+    const { videoId } = video.id.videoId; // will give linting error - read it and decide for yourself
     // {videoId} = video.id // is example of destructuring
     const url = `https://www.youtube.com/embed/${videoId}`;
 
     return (
       <div id="video-detail">
         <div className="embed-responsive embed-responsive-16by9">
-          <iframe className="embed-responsive-item" src={url} />
+          <iframe title="video_detail" className="embed-responsive-item" src={url} />
         </div>
         <div className="details">
           <div>{video.snippet.title}</div>
